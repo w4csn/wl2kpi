2f784e5962eded2ebf1ba15ad5484680de93cd66
w4csn <34305623+w4csn@users.noreply.github.com>
2017-12-06 15:11:29 -0500

    Add files via upload

---
+++ ax25/ax25_install.sh
@@ -0,0 +1,235 @@
+#!/bin/bash
+# ax25_install.sh
+# It will Download and Install AX25
+set -u # Exit if there are uninitialized variables.
+scriptname="`basename $0`"
+WL2KPI_INSTALL_LOGFILE="/var/log/wl2kpi_install.log"
+START_DIR=$(pwd)
+
+#Constants
+wd=$(pwd)
+uid=$(id -u)
+INST_UID=pi
+#LIBAX25=linuxax25-master/libax25/
+#TOOLS=linuxax25-master/ax25tools/
+#APPS=linuxax25-master/ax25apps/
+LIBAX25=linuxax25/libax25/
+TOOLS=linuxax25/ax25tools/
+APPS=linuxax25/ax25apps/
+AX25REPO=https://github.com/ve7fet/linuxax25
+	
+
+function Chk_Root {
+# Check for Root
+if [[ $EUID != 0 ]] ; then
+   echo "Must be root"
+    exit 1
+fi
+}
+
+function CreateAx25_Folders {
+if [ ! -d /usr/local/etc ]; then
+	echo "=== Creating file folders for Config files"
+	mkdir /usr/local/etc
+fi
+
+mkdir /usr/local/etc/ax25
+
+if [ ! -d /usr/local/var/ ]; then
+	echo "=== Creating file folders for Data files"
+	mkdir /usr/local/var
+fi
+
+mkdir /usr/local/var/ax25
+
+if [ ! -d /usr/etc/ax25 ]; then
+	rm -rf /etc/ax25
+fi
+
+if [ ! -d /usr/local/src ]; then
+	echo "=== Creating file folders for source files"
+	mkdir /usr/local/src
+	mkdir /usr/local/src/ax25
+else
+  mkdir /usr/local/src/ax25
+fi
+
+echo "=== Creating symlinks to standard directories"
+ln -s /usr/local/var/ax25/ /var/ax25
+ln -s /usr/local/etc/ax25/ /etc/ax25
+
+if [ -f /usr/lib/libax25.a ]; then
+	echo -e "\t Moving Old Libax25 files out of the way"
+	mkdir /usr/lib/ax25lib
+	mv /usr/lib/libax25* /usr/lib/ax25lib/
+fi
+}
+
+
+function DownloadAx25 {
+cd /usr/local/src/ax25
+echo "=== Downloading AX25 archives"
+if [ ! -d linuxax25 ]; then
+  echo -e "\t Downloading AX25 source"
+  git clone $AX25REPO .
+else
+  echo -e "\t Updating local AX25 source"
+  git pull $AX25REPO .
+fi
+}
+
+function Configure_libax25 {
+echo -e "=== Libax25 - Runtime Library files"
+echo -e "\t Creating Makefiles for Ax25lib."
+cd /usr/local/src/ax25/$LIBAX25
+./autogen.sh > liberror.txt 2>&1
+./configure >> liberror.txt 2>&1
+if [ $? -ne 0 ]; then
+    echo -e "\t Libax25 Configuration error - See liberror.txt"
+    exit 1
+fi
+}
+
+function CompileAx25 {
+echo "=== Compiling AX.25 Libraries"
+# Clean old binaries
+make clean > /dev/null
+
+# Compile
+echo -e "\t Compiling Runtime Lib files"
+make > liberror.txt 2>&1
+if [ $? -ne 0 ]
+    then
+    echo -e "\t Libax25 Compile error - See liberror.txt"
+    exit 1
+else   
+    echo -e "\t Libax25 Compiled"	
+fi
+
+# Install
+echo -e "\t Installing Runtime Lib files"
+make install >> liberror.txt 2>&1
+if [ $? -ne 0 ]; then
+    echo -e "Libax25 Install error - See liberror.txt"
+    exit 1
+else   
+    echo -e "\t Libax25 Installed"
+    rm liberror.txt
+fi
+
+# AX25 libraries declaration (into ld.so.conf)
+echo "/usr/local/lib" >> /etc/ld.so.conf && /sbin/ldconfig
+
+# Ax25-Apps
+echo -e "=== Compiling AX.25 Applications"
+cd /usr/local/src/ax25/$APPS
+echo -e "\t Creating Makefiles for AX25apps"
+./autogen.sh >  appserror.txt 2>&1
+./configure >> appserror.txt 2>&1
+
+# Clear old binaries
+make clean > /dev/null
+
+# Compile Ax25-apps
+echo -e "\t Compiling Ax25 apps"
+make > appserror.txt 2>&1
+if [ $? -ne 0 ]; then
+    echo -e "\t Ax25-Apps Compile Error - see appserror.txt"
+    exit 1
+fi
+
+# Install Ax25-apps
+echo -e "\t Installing Ax25 apps"
+make  install >> appserror.txt 2>&1
+if [ $? -ne 0 ]; then
+    echo -e "Ax25-Apps Install Error - see appserror.txt"
+    exit 1
+else
+    echo -e "\t Ax25-apps Installed"
+    rm appserror.txt
+fi
+
+# Ax25-tools
+echo -e "=== Compiling AX.25 Tools"
+cd /usr/local/src/ax25/$TOOLS
+echo -e "\t Creating Makefiles for AX25tools"
+./autogen.sh > toolserror.txt 2>&1
+./configure >> toolserror.txt 2>&1
+
+# Clear old binaries
+make clean > /dev/null
+
+# Compile Ax.25 tools
+echo -e " \t Compiling AX.25 tools"
+make > toolserror.txt 2>&1
+if [ $? -ne 0 ]; then
+    echo -e " ${Red} \t AX.25 tools Compile error - See toolserror.txt ${Reset}"
+    exit 1
+fi
+
+# Install Ax.25 tools
+echo -e "\t Installing AX.25 tools"
+make install >> toolserror.txt 2>&1
+if [ $? -ne 0 ]; then
+    echo -e "\t AX.25 tools Install error - See toolserror.txt"
+    exit 1
+else
+    echo -e "\t AX.25 tools Installed"
+    rm toolserror.txt
+fi
+}
+
+function FinishAx25_Install {
+# Set permissions for /usr/local/sbin/ and /usr/local/bin
+cd /usr/local/sbin/
+chmod 4775 *
+cd /usr/local/bin/
+chmod 4775 *
+echo -e "=== Ax.25 Libraries, Applications and Tools were successfully installed"
+
+# Add Modules to /etc/modules
+echo "ax25" >> /etc/modules
+echo "rose" >> /etc/modules
+echo "mkiss" >> /etc/modules
+
+# download start up files
+cd $wd/k4gbb
+wget -qt3 http://k4gbb.no-ip.info/docs/scripts/ax25
+wget -qt3 http://k4gbb.no-ip.info/docs/rpi/ax25-up.pi
+wget -qt3 http://k4gbb.no-ip.info/docs/scripts/ax25-down
+wget -qt3 http://k4gbb.no-ip.info/docs/rpi/axports
+wget -qt3 http://k4gbb.no-ip.info/docs/rpi/ax25d.conf
+wget -qt3 http://k4gbb.no.info/docs/rpi/calibrate_pi
+wget -qt3 http://k4gbb.no.info/docs/rpi/i2ckiss
+
+  
+cp $wd/k4gbb/ax25 /etc/init.d/ax25 && ln -s /etc/init.d/ax25 /usr/sbin/ax25
+chmod 755 /etc/init.d/ax25
+update-rc.d ax25 defaults
+# Add ax25 systemd service code here
+cd /etc/ax25
+cp $wd/k4gbb/ax25-up.pi /etc/ax25/ax25-up 
+cp $wd/k4gbb/ax25-down /etc/ax25/ax25-down && chmod 755 ax25-*
+cp $wd/k4gbb/axports /etc/ax25/axports
+cp $wd/k4gbb/ax25d.conf /etc/ax25/ax25d.conf
+touch nrports rsports
+}
+
+# Main
+echo "$(date "+%Y %m %d %T %Z"): $scriptname: script START" >> $WL2KPI_INSTALL_LOGFILE
+echo
+echo "$scriptname: script STARTED"
+echo
+
+Chk_Root
+CreateAx25_Folders
+DownloadAx25
+Configure_libax25
+CompileAx25
+FinishAx25_Install
+
+echo "$(date "+%Y %m %d %T %Z"): $scriptname: script FINISHED" >> $WL2KPI_INSTALL_LOGFILE
+echo
+echo "$scriptname: script FINISHED"
+echo
+# (End of Script)
---
+++ config/core_config.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+#
+# Run this script after core_install.sh
+#
+# Uncomment this statement for debug echos
+DEBUG=1
+set -u # Exit if there are uninitialized variables.
+
+scriptname="`basename $0`"
+WL2KPI_INSTALL_LOGFILE="/var/log/wl2kpi_install.log"
+START_DIR=$(pwd)
+
+# trap ctrl-c and call function ctrl_c()
+trap ctrl_c INT
+function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
+
+# ===== function ctrl_c trap handler
+function ctrl_c() { 
+echo "Exiting script from trapped CTRL-C"	
+exit
+}
+
+# ==== main
+echo "$(date "+%Y %m %d %T %Z"): $scriptname: script START" >> $WL2KPI_INSTALL_LOGFILE
+echo
+echo "$scriptname: script STARTED"
+echo
+
+# Be sure we're running as root
+if [[ $EUID != 0 ]] ; then
+   echo "Must be root"
+    exit 1
+fi
+
+echo " === Verify not using default password"
+# is there even a user pi?
+ls /home | grep pi > /dev/null 2>&1
+if [ $? -eq 0 ] ; then
+   echo "User pi found"
+   echo "Determine if default password is being used"
+
+   # get salt
+   SALT=$(grep -i pi /etc/shadow | awk -F\$ '{print $3}')
+
+   PASSGEN=$(mkpasswd --method=sha-512 --salt=$SALT raspberry)
+   PASSFILE=$(grep -i pi /etc/shadow | cut -d ':' -f2)
+
+#   dbgecho "SALT: $SALT"
+#   dbgecho "pass file: $PASSFILE"
+#   dbgecho "pass  gen: $PASSGEN"
+
+   if [ "$PASSFILE" = "$PASSGEN" ] ; then
+      echo "User pi is using default password"
+      echo "Need to change your password for user pi NOW"
+      read -t 1 -n 10000 discard
+      passwd pi
+      if [ $? -ne 0 ] ; then
+         echo "Failed to set password, exiting"
+	 exit 1
+      fi
+   else
+      echo "User pi not using default password."
+   fi
+
+else
+   echo "User pi NOT found"
+fi
+
+# Check hostname
+echo " === Verify hostname"
+HOSTNAME=$(cat /etc/hostname | tail -1)
+dbgecho "$scriptname: Current hostname: $HOSTNAME"
+
+if [ "$HOSTNAME" = "raspberrypi" ] || [ "$HOSTNAME" = "compass" ] ; then
+   # Change hostname
+   echo "Using default host name: $HOSTNAME, change it"
+   echo "Enter new host name followed by [enter]:"
+   read -t 1 -n 10000 discard
+   read -e HOSTNAME
+   echo "$HOSTNAME" > /etc/hostname
+fi
+
+# Get hostname again incase it was changed
+HOSTNAME=$(cat /etc/hostname | tail -1)
+
+echo "=== Set mail hostname"
+echo "$HOSTNAME.localhost" > /etc/mailname
+
+grep "127.0.1.1" /etc/hosts
+if [ $? -eq 0 ] ; then
+   # Found 127.0.1.1 entry
+   # Be sure hostnames match
+   HOSTNAME_CHECK=$(grep "127.0.1.1" /etc/hosts | awk {'print $2'})
+   if [ "$HOSTNAME" != "$HOSTNAME_CHECK" ] ; then
+      echo "Make host names match between /etc/hostname & /etc/hosts"
+      sed -i -e "/127.0.1.1/ s/127.0.1.1\t.*/127.0.1.1\t$HOSTNAME ${HOSTNAME}.localnet/" /etc/hosts
+   else
+      echo "host names match between /etc/hostname & /etc/hosts"
+   fi
+else
+   # Add a 127.0.1.1 entry to /etc/hosts
+   sed -i '1i\'"127.0.1.1\t$HOSTNAME $HOSTNAME.localnet" /etc/hosts
+   if [ $? -ne 0 ] ; then
+      echo "Failed to modify /etc/hosts file"
+   fi
+fi
+
+DATETZ=$(date +%Z)
+dbgecho "Time zone: $DATETZ"
+
+if [ "$DATETZ" == "UTC" ] ; then
+   echo " === Set time zone"
+   echo " ie. select America, then scroll down to 'Los Angeles'"
+   echo " then hit tab & return ... wait for it"
+   # pause to read above msg
+   sleep 4
+   dpkg-reconfigure tzdata
+fi
+
+cd $START_DIR
+
+echo "$(date "+%Y %m %d %T %Z"): $scriptname: script FINISHED" >> $WL2KPI_INSTALL_LOGFILE
+echo
+echo "$scriptname: script FINISHED"
+echo
