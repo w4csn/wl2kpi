e8ab9fa3a53fd09ce948f6ffc5ab3f3e3e558ed0
Scott Newton <snewton86@gmail.com>
2017-12-12 10:36:37 -0500

    core_functions.sh
    updates

--- core/core_functions.sh
+++ core/core_functions.sh
@@ -4,6 +4,12 @@
 # ===== function dbecho
 function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
 
+# ===== function ctrl_c trap handler
+function ctrl_c() {
+        echo "Exiting script from trapped CTRL-C"
+	exit
+}
+
 # ===== function chk_root
 function chk_root {
 # Check for Root
@@ -12,4 +18,11 @@ if [[ $EUID != 0 ]] ; then
    exit 1
 fi
 }
+
+# ===== function is_pkg_installed
+function is_pkg_installed() {
+
+return $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed" >/dev/null 2>&1)
+}
+
 # ===== End Function List =====
