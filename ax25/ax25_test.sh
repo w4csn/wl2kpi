05a7864d6fd2d96c08653884c7e149062985934b
Scott Newton <snewton86@gmail.com>
2017-12-23 20:07:26 -0500

    update

--- ax25/ax25_test.sh
+++ ax25/ax25_test.sh
@@ -9,6 +9,59 @@ DEBUG=1
 set -u # Exit if there are uninitialized variables.
 scriptname="`basename $0`"
 source $START_DIR/core/core_functions.sh
+
+# ===== Function List =====
+
+function CreateAx25_Folders {
+echo -e "\t${Cyan)=== Creating file folders necessary for Ax.25${Reset}"
+if [ ! -d "/usr/local/etc" ]; then
+   echo -e "\t\t Creating file folders for Config files."
+   mkdir /usr/local/etc
+fi
+
+if [ ! -d "/usr/local/etc/ax25" ]; then
+   mkdir /usr/local/etc/ax25
+fi
+
+if [ ! -d "/usr/local/var/" ]; then
+	echo -e "\t\t Creating file folders for Data files."
+	mkdir /usr/local/var
+fi
+
+if [ ! -d "/usr/local/var/ax25" ]; then
+   mkdir /usr/local/var/ax25
+fi
+
+if [ ! -d "/usr/etc/ax25" ]; then
+   rm -rf /etc/ax25
+fi
+
+if [ ! -d /usr/local/src ]; then
+   echo -e "\t\t Creating /usr/local/src"
+   mkdir /usr/local/src
+fi
+if [ ! -d /usr/local/src ]; then
+   echo -e "\t\t Creating /usr/local/src/ax25"
+   mkdir /usr/local/src/ax25
+fi
+mkdir /usr/local/src/ax25
+echo -e "\t=== Creating symlinks to standard directories"
+if [ ! -L /var/ax25 ]; then
+   ln -s /usr/local/var/ax25/ /var/ax25
+fi
+if [ ! -L /etc/ax25 ]; then
+   ln -s /usr/local/etc/ax25/ /etc/ax25
+fi
+
+if [ -f /usr/lib/libax25.a ]; then
+	echo -e "\t\t Moving Old Libax25 files out of the way"
+	mkdir /usr/lib/ax25lib
+	mv /usr/lib/libax25* /usr/lib/ax25lib/
+fi
+}
+
+# ===== End Function List
+
 # ===== Main =====
 sleep 3
 clear
@@ -21,7 +74,7 @@ echo
 chk_root
 
 # Set up folder structure
-#CreateAx25_Folders
+CreateAx25_Folders
 
 # Download source files
 #DownloadAx25
