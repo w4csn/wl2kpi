18ec38a7b4fac1c2a1bdbe6ae36f01fa73e73250
Scott Newton <snewton86@gmail.com>
2018-01-21 23:04:54 -0500

    ax25_config.sh
    script cleanup

--- ax25/ax25_config.sh
+++ ax25/ax25_config.sh
@@ -17,66 +17,73 @@ AX25_CFGDIR="/etc/ax25"
 
 # ===== Function List =====
 
-# ===== function get_callsign
-function get_callsign() { 
-
+function get_callsign()
+{ 
 # Check if call sign var has already been set
 if [ "$CALLSIGN" == "N0ONE" ] ; then
-
    read -t 1 -n 10000 discard
    echo "Enter call sign, followed by [enter]:"
    read CALLSIGN
-
    sizecallstr=${#CALLSIGN}
-
    if (( sizecallstr > 6 )) || ((sizecallstr < 3 )) ; then
       echo "Invalid call sign: $CALLSIGN, length = $sizecallstr"
       return 0
    fi
-
    # Convert callsign to upper case
    CALLSIGN=$(echo "$CALLSIGN" | tr '[a-z]' '[A-Z]')
 fi
-
 dbgecho "Using CALL SIGN: $CALLSIGN"
 return 1
 }
 
-# ===== function get_ssid
-function get_ssid() {
-
+function get_ssid()
+{
 read -t 1 -n 10000 discard
 echo "Enter ssid (0 - 15) followed by [enter]:"
 read -e SSID
-
 if [ -z "${SSID##*[!0-9]*}" ] ; then
    echo "Input: $SSID, not a positive integer"
    return 0
 fi
-
 sizessidstr=${#SSID}
-
 if (( sizessidstr > 2 )) || ((sizessidstr < 0 )) ; then
    echo "Invalid ssid: $SSID, length = $sizessidstr, should be 1 or 2 numbers"
    return 0
 fi
-
 dbgecho "Using SSID: $SSID"
 return 1
 }
 
-# ===== function prompt_read
-function prompt_read() {
+function prompt_read()
+{
 while get_callsign ; do
   echo -e "Input ${Red}error${Reset}, try again"
 done
-
 while get_ssid ; do
   echo -e "Input ${Red}error${Reset}, try again"
 done
 }
 
-function configure_axports() {
+function chk_config_dir()
+{
+# check if /etc/ax25 exists as a directory or symbolic link
+echo -e "Checking for /etc/ax25 symbolic link"
+if [ ! -d "/etc/ax25" ] || [ ! -L "/etc/ax25" ] ; then
+   if [ ! -d "/usr/local/etc/ax25" ] ; then
+	  echo -e "Directory /usr/local/etc/ax25 not found..."
+      echo -e "AX.25 ${Red}NOT${Reset} Properly installed, make sure that ax25_install.sh ran successfully"
+      exit 1
+   else
+      echo -e "Creating symbolic link /etc/ax25"
+      ln -s /usr/local/etc/ax25 /etc/ax25
+   fi
+else
+   echo -e "Found /etc/ax25 symbolic link"
+fi
+}
+
+function configure_axports()
+{
 	sed -i -e "/k4gbb/ /k4gbb-1/$callsign/ " /etc/ax25/axports
 }
 
@@ -92,44 +99,22 @@ echo
 
 # Be sure we're running as root
 chk_root
-
-# Unneeded Check
-#if [ ! -f "/etc/ax25/axports" ]; then
-#   echo -e "AX.25 ${Red}NOT${Reset} Installed, make sure that ax25_install.sh ran successfully"
-#   exit 1
-#fi
-
-# check if /etc/ax25 exists as a directory or symbolic link
-if [ ! -d "/etc/ax25" ] || [ ! -L "/etc/ax25" ] ; then
-   if [ ! -d "/usr/local/etc/ax25" ] ; then
-      echo -e "AX.25 ${Red}NOT${Reset} Installed, make sure that ax25_install.sh ran successfully"
-      exit 1
-   else
-      echo "Making symbolic link to /etc/ax25"
-      ln -s /usr/local/etc/ax25 /etc/ax25
-   fi
-else
-   echo " Found ax.25 link or directory"
-fi
-
+chk_config_dir
 # if there are any args on command line assume it's a callsign
 if (( $# != 0 )) ; then
    CALLSIGN="$1"
 fi
 
-# Check for a valid callsign
-get_callsign
-
-# === Configure axports
+# Configure axports
 echo -e "${Cyan}=== Configuring axports${Reset}"
 echo
 grep -i "$AX25PORT" $AX25_CFGDIR/axports
 if [ $? -eq 1 ] ; then
    echo
-   echo -e "\t No ax25 ports defined"
+   echo -e " No AX.25 ports defined"
    echo
    mv $AX25_CFGDIR/axports $AX25_CFGDIR/axports-dist
-   echo -e "\t Original ax25 axports saved as axports-dist"
+   echo -e " Original ax25 axports saved as axports-dist"
    echo
    prompt_read
 {
@@ -140,8 +125,7 @@ echo "# portname	callsign	speed	paclen	window	description"
 echo "$AX25PORT            $CALLSIGN-$SSID         19200    256     7       TNC-Pi port"
 } > $AX25_CFGDIR/axports
 else
-   echo -e "\t AX.25 port $AX25PORT already configured"
-   echo
+   echo -e " AX.25 port $AX25PORT already configured"
 fi
 echo -e "${Cyan}=== axports Configuration ${Green}Finished${Reset}"
 echo
@@ -150,7 +134,6 @@ echo
 # Set up a listening socket, for testing
 # Make it different than previous SSID
 echo -e "${Cyan}=== Configuring ax25d.conf${Reset}"
-echo
 if ((SSID < 15)) ; then
    AX25DSSID=$((SSID+1))
 else
@@ -159,10 +142,10 @@ fi
 
 grep  "n0one" /etc/ax25/ax25d.conf >/dev/null
 if [ $? -eq 0 ] ; then
-   echo -e "\t ax25d.conf not configured"
+   echo -e " ax25d.conf not configured"
    echo
    mv $AX25_CFGDIR/ax25d.conf $AX25_CFGDIR/ax25d.conf-dist
-   echo -e "\t Original ax25d.conf saved as ax25d.conf-dist"
+   echo -e " Original ax25d.conf saved as ax25d.conf-dist"
    echo
    # copy first 1 line of file
    sed -n '1p' $AX25_CFGDIR/ax25d.conf-dist >> $AX25_CFGDIR/ax25d.conf
@@ -174,7 +157,7 @@ echo "default  * * * * * *  - root /usr/sbin/ttylinkd ttylinkd"
 } > $AX25_CFGDIR/ax25d.conf
    sed -n '$p' $AX25_CFGDIR/ax25d.conf-dist >> $AX25_CFGDIR/ax25d.conf
 else
-   echo -e "\t ax25d.conf already configured"
+   echo -e " ax25d.conf already configured"
    echo
 fi
 echo -e "${Cyan}=== Configuration ${Green}Finished${Reset}"
