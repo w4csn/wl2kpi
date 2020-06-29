7e1372c8563e38588a6dda9459a25439f4505352
Scott Newton <snewton86@gmail.com>
2020-06-29 12:54:47 -0400

    Update core_functions.sh
    update

--- core/core_functions.sh
+++ core/core_functions.sh
@@ -138,7 +138,7 @@ while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
     local temp="${spinstr#?}"
     printf " [%c]  " "$spinstr"
     local spinstr=$temp${spinstr%"$temp"}
-    sleep $delay
+    sleep "${delay}"
     printf "\b\b\b\b\b\b"
 done
 printf "    \b\b\b\b"
