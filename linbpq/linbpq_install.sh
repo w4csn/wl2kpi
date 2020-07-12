b651eedd90a0804c6c77510834ca415e65d5b07a
Scott Newton <snewton86@gmail.com>
2020-07-12 15:59:44 -0400

    Update linbpq_install.sh

---
+++ linbpq/linbpq_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Installs/Updates LinBPQ
+# 
+#
+# Uncomment this statement for debug echos
+DEBUG=1
+set -u # Exit if there are uninitialized variables.
+source $START_DIR/core/core_functions.sh
+
+
+# ===== Function List =====
+
+# ===== End of Functions list =====
+
+# ===== Main
+
+echo
+echo -e "${BluW}$scriptname: script FINISHED${Reset}"
+echo
