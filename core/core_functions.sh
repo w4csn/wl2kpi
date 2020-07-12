eff45439a3640f3cbe5ef8680578cd274d90c188
Scott Newton <snewton86@gmail.com>
2020-07-12 16:40:54 -0400

    Update core_functions.sh

--- core/core_functions.sh
+++ core/core_functions.sh
@@ -150,20 +150,21 @@ systemctl is-active $service_name >/dev/null
    fi
 }
 
-function spinner() {
-local -r pid="${1}"
-local -r delay='0.75'
-local spinstr='|/-\'
-local temp
-while ps a | awk '{print $1}' | grep -q "${pid}"; do
-    temp="${spinstr#?}"
-    printf " [%c]  " "${spinstr}"
-    spinstr=${temp}${spinstr%"${temp}"}
-    sleep "${delay}"
-    printf "\b\b\b\b\b\b"
-done
-printf "    \b\b\b\b"
-}
+# function spinner -- BROKEN!!
+#function spinner() {
+#local -r pid="${1}"
+#local -r delay='0.75'
+#local spinstr='|/-\'
+#local temp
+#while ps a | awk '{print $1}' | grep -q "${pid}"; do
+#    temp="${spinstr#?}"
+#    printf " [%c]  " "${spinstr}"
+#    spinstr=${temp}${spinstr%"${temp}"}
+#    sleep "${delay}"
+#    printf "\b\b\b\b\b\b"
+#done
+#printf "    \b\b\b\b"
+#}
 
 
 # ===== End Function List =====
