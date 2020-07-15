bb3779324f31265e9e1a5e25723eb14eb88536b0
Scott Newton <snewton86@gmail.com>
2020-07-15 10:04:45 -0400

    Update app_install.sh

--- app_install.sh
+++ app_install.sh
@@ -94,7 +94,7 @@ do
 	echo -e "${Green}core${Reset}        Install CORE (Do this first!)"
 	echo -e "${Green}ax25${Reset}        Install AX.25"
 	echo -e "${Red}Direwolf${Reset}    Install Direwolf"
-	echo -e "${Green}linbpq${Reset}      Install LinBPQ"
+	echo -e "${Green}pilinbpq${Reset}      Install PiLinBPQ"
 	echo -e "${Green}rmsgw${Reset}       Install RMS Gateway"
 	echo -e "${Red}plu${Reset}         Install paclink-unix  basic "
 	echo -e "${Red}pluimap${Reset}     Install paclink-unix with imap "
@@ -136,14 +136,14 @@ do
 		linbpq)
 			echo -e "${BluW}$scriptname: Install LinBPQ${Reset}"
 			# install pilinbpq
-			pushd $START_DIR/linbpq > /dev/null
-			scriptname="linbpq_install"
-			source ./linbpq_install.sh
-			scriptname="linbpq_config"
-			source ./linbpq_config.sh
+			pushd $START_DIR/pilinbpq > /dev/null
+			scriptname="pilinbpq_install"
+			source ./pilinbpq_install.sh
+			scriptname="pilinbpq_config"
+			source ./pilinbpq_config.sh
 			popd > /dev/null
 			scriptname="`basename $0`"
-			echo -e "${BluW}$scriptname: LinBPQ installation FINISHED${Reset}"
+			echo -e "${BluW}$scriptname: PiLinBPQ installation FINISHED${Reset}"
 			echo
 			read -n 1 -s -r -p "Press any key to continue"
 		;;
