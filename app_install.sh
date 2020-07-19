3b0969c0636c32dde2b4b0ca0049cf30da16f3a7
Scott Newton <snewton86@gmail.com>
2020-07-18 20:51:16 -0400

    Update app_install.sh

--- app_install.sh
+++ app_install.sh
@@ -6,8 +6,8 @@
 #
 # Uncomment this statement for debug echos
 DEBUG=1
-set +u # Exit if there are uninitialized variables.
-set +a
+#set +u # Exit if there are uninitialized variables.
+#set +a
 scriptname="`basename $0`"
 WL2KPI_INSTALL_LOGFILE="/home/pi/Scripts/Temp/wl2kpi_install.log"
 START_DIR=$(pwd)
